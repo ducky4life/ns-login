e02f4f67d39f28f185116291e977c318e05cee5d
Ducky <124077142+ducky4life@users.noreply.github.com>
2025-05-28 03:46:02 +0800

    Add files via upload

---
+++ login.py
@@ -0,0 +1,25 @@
+import csv
+from nsdotpy.session import NSSession
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+UA = "ducky"
+
+session = NSSession("autologin", "1.0.0", "Ducky", UA)
+rowcount = 0
+rows = len(list(csv.reader(open('input.csv'))))
+
+if UA != "":
+	with open('input.csv') as csv_file:
+		csv_reader = csv.reader(csv_file)
+		for row in csv_reader:
+			rowcount+= 1
+			print(f"({rowcount}/{rows}) logging into {row[0]}...")
+			try:
+				pwd = row[1]
+			except IndexError:
+				pwd = str(os.getenv("PASSWORD"))
+			session.api_request("nation", target=row[0], shard="ping", password=pwd)
+		print("finished pinging!")
